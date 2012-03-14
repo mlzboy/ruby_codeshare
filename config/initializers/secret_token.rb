33ff7bd0f51f43504e368784af4bafd5b95cc988
unknown <Administrator@maolingzhi-360buy.(none)>
2012-03-15 07:45:50 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeShare::Application.config.secret_token = '2300dd5b6d42648daa543e9e3105d6c8eea822a8c41ace7a62ad5caa92d333c1a26757cc4506d2a7027aa3933c5691b1850b291c91e77908681dd7c2a36f3b41'
